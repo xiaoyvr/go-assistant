dca12602d5afa4e4bb5a0f5df3433f2c407f0f6a
Your Name <you@example.com>
2014-02-25 09:32:13 +0800

    remove comments

--- plugin/main.js
+++ plugin/main.js
@@ -3,10 +3,8 @@ var id = chrome.contextMenus.create({
 	"contexts":['link'], 
 	"onclick": function (info, tab) {
  		chrome.tabs.sendRequest(tab.id, 'mstsc', function(text){
-	  		console.log(text);
 	  		if (text) {
 		  		var ip = text.match(/ip:([.\d]*)/)[1]
-		  		console.log(ip);
 		  		if (!!ip) {
 					chrome.runtime.sendNativeMessage("com.thoughtworks.mstsc", {
 						"cmd" : "c:\\windows\\system32\\mstsc.exe",
